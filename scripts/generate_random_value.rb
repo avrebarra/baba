5a70b124f38e5dfe60baeb74907f33725c0c4c4a
avrebarra <avrebarra@users.noreply.github.com>
2024-05-09 04:33:46 +0000

    deploy: 225bc4a4e04c842289d76a46d04f46a80b0a8231

--- scripts/generate_random_value.rb
+++
@@ -1,6 +0,0 @@
-values = ARGV[0].split("/")
-weighted_values = values.map { |v| v.split("@") }
-weighted_values.map! { |v| v.length > 1 ? [v[0], v[1].to_i] : [v[0], 1] }.flatten(1)
-weighted_values = weighted_values.map { |v| [v[0]] * v[1]}.flatten
-random_value = weighted_values.sample
-puts random_value
