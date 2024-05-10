a7c9c070a060e9cb575ae4826e85ad2e605dd9f4
Avre Barra <avrebarra@gmail.com>
2024-05-10 13:50:08 +0700

    fix flaky generator

--- _builder/libs/babba.rb
+++ _builder/libs/babba.rb
@@ -12,6 +12,19 @@ LANG_CODE = {
 }
 
 module Babba
+  def self.clean_yaml(yaml_string)
+    yaml_string
+      .split("\n")
+      .map do |line|
+        if line.strip.start_with?("- ")
+          "- \"#{line.strip[2..-1].gsub(/"/, '\\"')}\""
+        else
+          line
+        end
+      end
+      .join("\n")
+  end
+
   def self.generate_fable(
     openai_api_key:,
     language_complexity: 3,
@@ -20,24 +33,30 @@ module Babba
     hint_mood: nil,
     hint_character: nil
   )
-    YAML.load(
-      OpenAI.prompt(
-        openai_api_key,
-        "You work on company generating fables to learn foreign language. Compact answer in YAML.",
-        <<~PROMPT
-          Create original bitsized fable in English within #{words_length_range} words. Style it like light, fun, Duolingo stories.
-          Write in vocabulary complexity suitable for #{language_complexity}yo local speaker.
-          Use basic, descriptive, clear sentence structures. Give dialogues. Give spectacular conflict, climax, & resolution. Structure into paragraphs.
-          Also generate moral message of the story. But do not force moral messages in story paragraph.
-          Also generate story hook with curiosity hooking question (max 3 sentences) for story overview.
-          Also generate list of keywords for good vocabulary enriching.
-          #{"Use #{hint_character} as character." unless hint_character.nil?}
-          #{"Use #{hint_mood} as story mood." unless hint_mood.nil?}
-          #{"Use #{hint_cultural_influence} as cultural influence." unless hint_cultural_influence.nil?}
-          Output Format: YAML {title: string, hook: string, moral: string, paragraphs: string[], keywords: string[10-15 words]} (MUST ensure valid YAML object structure!)
-        PROMPT
+    out =
+      clean_yaml(
+        OpenAI.prompt(
+          openai_api_key,
+          "You work on fable-generating company for all ages. Compact answer in YAML.",
+          <<~PROMPT
+            Create bitsize fable in English within #{words_length_range} words. Make it a lightread. Style it like Duolingo story.
+            Give dialogues. Give unique and original conflicts & resolutions. Give bit more details on the conflict resolutions. Use paragraphs.
+            Use vocabulary suitable for #{language_complexity}yo local speaker.
+            Use basic & descriptive sentence structures.
+            #{"Use #{hint_character} as character." unless hint_character.nil?}
+            #{"Use #{hint_mood} as story mood." unless hint_mood.nil?}
+            #{"Use #{hint_cultural_influence} as cultural influence." unless hint_cultural_influence.nil?}
+
+            Create light non-cheesy moral message of the story. Don't force the moral messages in story paragraph.
+            Create story hook with curiosity hooking question (max 3 sentences) for story overview.
+            Create list of keywords for good vocabulary enriching.
+
+            Use Output Format: YAML {title: string, hook: string, moral: string, paragraphs: string[], keywords: string[10-15 words]}
+          PROMPT
+        )
       )
-    )
+
+    YAML.load out
   end
 
   def self.generate_translation(
@@ -137,37 +156,44 @@ module Babba
     use_linguafranca: true,
     extract_keyterms: 0
   )
-    YAML.load OpenAI.prompt(
-                openai_api_key,
-                "You work on company generating fables to learn foreign language. Compact answer in YAML.",
-                <<~PROMPT
-                  Translate text #{"from #{from_lang}" if from_lang} to #{to_lang}. #{"Literal word-by-word translations." if use_literal}
-                  Use vocabulary & sentence structure suitable for #{language_complexity}yo #{to_lang} people.
-                  Never change/translate character's names.
-                  #{"Generate a list of #{to_lang} keywords (#{extract_keyterms}) used in translation to enrich vocabulary." if extract_keyterms > 0}
-                  #{"Adjust text to prioritize comprehensibility for age. Use lingua-francas." if use_linguafranca}
-                  #{"Notes: #{notes}" unless notes.nil?}
-                  Output Format: YAML {translated: string#{", keywords: string[#{extract_keyterms}]" if extract_keyterms > 0}}
-                  MUST Use YAML literal block scalar syntax.
-                  Text to translate: #{text}
-                PROMPT
-              ).strip
+    out =
+      clean_yaml(
+        OpenAI.prompt(
+          openai_api_key,
+          "You work on company generating fables in multi languages. Compact answer in YAML.",
+          <<~PROMPT
+          Translate text #{"from #{from_lang}" if from_lang} to #{to_lang}. #{"Literal word-by-word translations." if use_literal}
+          Use vocabulary suitable for #{language_complexity}yo local speaker. #{"Use lingua-francas." if use_linguafranca}
+          Use basic & descriptive sentence structures.
+          Do not translate character names.
+          #{"Notes: #{notes}" unless notes.nil?}
+          #{"Extract #{extract_keyterms} keyterms from result. Without translations." if extract_keyterms > 0}
+          Output Format: YAML {translated: string (Use YAML literal block scalar)#{", keywords: string[]" if extract_keyterms > 0}}
+
+          Text to translate: #{text}
+        PROMPT
+        )
+      )
+    YAML.load out
   end
 
   def self.translate_keywords(openai_api_key:, words:, from: "en", to:)
     from = LANG_CODE[from]
     to = LANG_CODE[to]
 
-    JSON.parse OpenAI.prompt(
-                 openai_api_key,
-                 "You work on company generating fables to learn foreign language. Compact answer as JSON Hashmap.",
-                 <<~PROMPT
-                    Map these #{"#{from}" if from} words to their #{to} translations.
-                    Each word can be mapped to multiple words if it has synonymous translations.
-                    Do not add explanations!!!
-                    Words List: ['#{words.join("','")}']
-                    Output Format: Valid JSON HashMap<string, string[]> (map word to translated-words)
-                  PROMPT
-               )
+    out =
+      OpenAI.prompt(
+        openai_api_key,
+        "You work on company generating fables to learn foreign language. Compact answer as JSON Hashmap.",
+        <<~PROMPT
+          Map these #{"#{from}" if from} words to their #{to} translations.
+          Each word can be mapped to multiple words if it has synonymous translations.
+          Do not add explanations!!!
+          Words List: ['#{words.join("','")}']
+          Output Format: Valid JSON HashMap<string, string[]> (map word to translated-words)
+        PROMPT
+      )
+
+    JSON.parse out
   end
 end
